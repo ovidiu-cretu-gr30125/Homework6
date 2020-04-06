cdfed617d5b4e96f7a0319bdf11c42720133a0c2
Ovidiu Cretu <cretuovidiudaniel@yahoo.com>
2020-04-06 17:57:42 +0300

    Done1.1

--- src/main/java/isp/lab6/exercise1/Exercise1.java
+++ src/main/java/isp/lab6/exercise1/Exercise1.java
@@ -10,12 +10,12 @@ public class Exercise1 {
         sensorCluster.addSensor("id2", SENSOR_TYPE.TEMPERATURE);
         sensorCluster.addSensor("id3", SENSOR_TYPE.PRESSURE);
 
-        sensorCluster.writeSensorReading("id2", 15, LocalDateTime.of(2020, 04, 02, 4, 35, 00));
-        sensorCluster.writeSensorReading("id1", 5, LocalDateTime.of(2019, 03, 02, 9, 45, 00));
-        sensorCluster.writeSensorReading("id2", 10, LocalDateTime.of(2020, 05, 02, 4, 34, 00));
-        sensorCluster.writeSensorReading("id3", 25, LocalDateTime.of(2020, 06, 02, 12, 20, 00));
-        sensorCluster.writeSensorReading("id3", 5, LocalDateTime.of(2020, 06, 02, 8, 15, 00));
-
+        sensorCluster.writeSensorReading("id2", 20, LocalDateTime.of(2019, 07, 02, 4, 35, 30));
+        sensorCluster.writeSensorReading("id1", 5, LocalDateTime.of(2019, 03, 22, 9, 45, 50));
+        sensorCluster.writeSensorReading("id2", 10, LocalDateTime.of(2018, 05, 02, 4, 34, 00));
+        sensorCluster.writeSensorReading("id3", 25, LocalDateTime.of(2020, 02, 20, 12, 20, 35));
+        sensorCluster.writeSensorReading("id3", 5, LocalDateTime.of(2017, 06, 10, 8, 15, 44));
+        System.out.println(sensorCluster.getSensorById("id1").toString());
         System.out.println(sensorCluster.toString());
         System.out.println("......................");
         sensorCluster.getSensorById("id2");
